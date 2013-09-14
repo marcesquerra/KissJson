f51ac47807ba58da388df9fb18de28e8fca6692f
Marc Esquerrà i Bayo <esquerra@w3d.cc>
2013-09-14 23:06:20 +0100

    Milestone1 published into MavenCentral

--- project/Build.scala
+++ project/Build.scala
@@ -6,11 +6,38 @@ object BuildSettings
 
 	val buildSettings = Defaults.defaultSettings ++ Seq(
 			organization := "com.bryghts.kissjson",
-			version      := "0.0.1-SNAPSHOT",
+			version      := "0.0.1-M1",
 			scalaVersion := "2.10.2",
 			scalacOptions ++= Seq(),
 			libraryDependencies += "com.bryghts.kissnumber" % "kissnumber_2.10" % "0.0.1",
-			libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-M4"
+			libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.10.2",
+			publishMavenStyle := true,
+			publishTo <<= version { (v: String) =>
+				val nexus = "https://oss.sonatype.org/"
+				if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
+				else                             Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+			},
+			publishArtifact in Test := false,
+//			useGpg := true,
+			pomExtra := (
+				<url>http://www.brights.com</url>
+				<licenses>
+					<license>
+					<name>mit</name>
+					</license>
+				</licenses>
+				<scm>
+					<url>git@github.com:marcesquerra/KissNumber.git</url>
+					<connection>scm:git:git@github.com:marcesquerra/KissNumber.git</connection>
+				</scm>
+				<developers>
+					<developer>
+						<name>Marc Esquerrà i Bayo</name>
+						<email>esquerra@bryghts.com</email>
+					</developer>
+				</developers>
+			),
+			pomIncludeRepository := { _ => false }
 	)
 
 }
---
+++ project/plugins.sbt
@@ -0,0 +1,1 @@
+resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
