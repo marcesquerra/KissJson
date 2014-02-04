1864e865a357d3e8c0fa09d7246f4dca52530110
Marc Esquerrà i Bayo <esquerra@w3d.cc>
2014-02-04 20:28:14 +0000

    Version 0.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object BuildSettings
 
 	val buildSettings = Defaults.defaultSettings ++ Seq(
 			organization := "com.bryghts.kissjson",
-			version      := "0.0.1-M5",
+			version      := "0.1.0",
 			scalaVersion := "2.10.3",
 			scalacOptions ++= Seq(),
 			libraryDependencies += "com.bryghts.kissnumber" % "kissnumber_2.10" % "0.0.3",
